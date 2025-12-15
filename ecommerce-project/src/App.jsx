032d841ad56628ec0c563ad05e4dc8ea9e97cb7c
= <=>
2025-12-15 21:45:35 +0100

    Move orders page to React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import { HomePage } from './Pages/HomePage';
 import {Routes, Route} from 'react-router';
 import { CheckoutPage } from './Pages/CheckoutPage';
+import { OrdersPage } from './Pages/OrdersPage';
 function App() {
   return (
     <>
     <Routes>
       <Route index element = {<HomePage />} />
       <Route path = "checkout" element = {<CheckoutPage />} />
+      <Route path = 'orders' element = {<OrdersPage />}></Route>
     </Routes>
     </>
   )
