1ff40faaced1ef597d8843a59b6fc999d4904459
= <=>
2025-12-16 13:43:28 +0100

    Use axios to fetch the data

--- ecommerce-project/src/Pages/HomePage.jsx
+++ ecommerce-project/src/Pages/HomePage.jsx
@@ -1,15 +1,14 @@
+import axios from 'axios'
 import './HomePage.css';
 import { Header } from '../Components/Header';
 import CheckMark from "../assets/images/icons/checkmark.png";
 import { products } from '../../starting-code/ecommerce-project-main/data/products';
 
 export function HomePage() {
-    fetch('http://localhost:3000/api/products')
+    axios.get('http://localhost:3000/api/products')
         .then((response) => {
-            return response.json()
-        }).then((data) => {
-            console.log(data)
-        });
+            console.log(response.data)
+        })
     return (
         <>
             <title>Ecommerce project</title>
