9466cf6a5f7f5b2c2e87c6c83a9d857abee62744
= <=>
2025-12-17 15:04:12 +0100

    Make reAddProduct feature

--- ecommerce-project/src/Pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/Pages/orders/OrdersGrid.jsx
@@ -1,16 +1,15 @@
 import dayjs from "dayjs"
 import { formatCurrency } from "../../utils/Money"
-import { Fragment } from "react"
-import { Header } from "../../Components/Header"
+import { OrderDetails } from "./OrderDetails"
 
-export function OrdersGrid({orders}) {
-    console.log(orders)
+
+export function OrdersGrid({ orders, loadCart }) {
+
     return (
         <div className="orders-grid">
             {orders.map((order) => {
                 return (
                     <div key={order.id} className="order-container">
-
                         <div className="order-header">
                             <div className="order-header-left-section">
                                 <div className="order-date">
@@ -29,41 +28,7 @@ export function OrdersGrid({orders}) {
                             </div>
                         </div>
 
-                        <div className="order-details-grid">
-                            {order.products.map((orderProduct) => {
-                                return (
-                                    <Fragment key={orderProduct.id}>
-                                        <div className="product-image-container">
-                                            <img src={orderProduct.product.image} />
-                                        </div>
-
-                                        <div className="product-details">
-                                            <div className="product-name">
-                                                {orderProduct.product.name}
-                                            </div>
-                                            <div className="product-delivery-date">
-                                                Arriving on: {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                                            </div>
-                                            <div className="product-quantity">
-                                                Quantity: {orderProduct.quantity}
-                                            </div>
-                                            <button className="buy-again-button button-primary">
-                                                <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                                                <span className="buy-again-message">Add to Cart</span>
-                                            </button>
-                                        </div>
-
-                                        <div className="product-actions">
-                                            <a href={`/tracking/${order.id}/${orderProduct.productId}`}>
-                                                <button className="track-package-button button-secondary">
-                                                    Track package
-                                                </button>
-                                            </a>
-                                        </div>
-                                    </Fragment>
-                                )
-                            })}
-                        </div>
+                        <OrderDetails order={order} loadCart={loadCart} />
                     </div>
                 )
             })}
